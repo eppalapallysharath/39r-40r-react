8fb19a42e01bbb4a1892d5f3719b96bdddf12572
eppalapallysharath <eppalapallysharath@gmail.com>
2025-10-13 18:10:27 +0530

    hello react

---
+++ hello_react/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  
+  return<div> <h1>hi hello world im react application</h1>
+    <p>iam sharath</p>
+    <div><p>wow</p></div>
+    </div>
+}
+
+export default App
